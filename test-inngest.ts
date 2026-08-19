4e998e522ab23295212f4461eaa44637e51a4fa8
franklinfranklinfranklin <franklinlivemail@gmail.com>
2026-08-19 19:46:06 +0200

    chore: Remove scratch test files that break production TS build

--- test-inngest.ts
+++
@@ -1,7 +0,0 @@
-import { Inngest } from "inngest";
-const inngest = new Inngest({ id: "my-app" });
-inngest.createFunction(
-  { id: "my-func" },
-  { event: "my.event" },
-  async ({ event, step }) => {}
-);
